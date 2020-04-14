3e8f63a04f0d46cb0b3376e5ad4965fa00252c15
isuru1221 <isurusandaruwann1221@gmail.com>
2020-04-14 23:18:10 +0530

    Add Basic Login Page & Configure AngularFire

--- Admin Panel/src/app/app.module.ts
+++ Admin Panel/src/app/app.module.ts
@@ -1,22 +1,29 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {LocationStrategy, PathLocationStrategy} from '@angular/common';
+import {AppRoutes} from './app.routing';
+import {AppComponent} from './app.component';
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { LocationStrategy, PathLocationStrategy } from '@angular/common';
-import { AppRoutes } from './app.routing';
-import { AppComponent } from './app.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {FullComponent} from './layouts/full/full.component';
+import {AppHeaderComponent} from './layouts/full/header/header.component';
+import {AppSidebarComponent} from './layouts/full/sidebar/sidebar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './demo-material-module';
 
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FullComponent } from './layouts/full/full.component';
-import { AppHeaderComponent } from './layouts/full/header/header.component';
-import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DemoMaterialModule } from './demo-material-module';
+import {SharedModule} from './shared/shared.module';
+import {SpinnerComponent} from './shared/spinner.component';
 
-import { SharedModule } from './shared/shared.module';
-import { SpinnerComponent } from './shared/spinner.component';
+// firebase
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {environment} from '../environments/environment';
+
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +31,8 @@ import { SpinnerComponent } from './shared/spinner.component';
     FullComponent,
     AppHeaderComponent,
     SpinnerComponent,
-    AppSidebarComponent
+    AppSidebarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -33,8 +41,14 @@ import { SpinnerComponent } from './shared/spinner.component';
     FormsModule,
     FlexLayoutModule,
     HttpClientModule,
+    // firebase_initialization
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    //
     SharedModule,
-    RouterModule.forRoot(AppRoutes)
+    RouterModule.forRoot(AppRoutes),
+    ReactiveFormsModule
   ],
   providers: [
     {
@@ -44,4 +58,5 @@ import { SpinnerComponent } from './shared/spinner.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- Admin Panel/src/app/app.routing.ts
+++ Admin Panel/src/app/app.routing.ts
@@ -1,26 +1,31 @@
 import { Routes } from '@angular/router';
 
 import { FullComponent } from './layouts/full/full.component';
+import { LoginComponent } from './login/login.component';
 
 export const AppRoutes: Routes = [
+  // {
+  //   path: '',
+  //   component: FullComponent,
+  //   children: [
+  //     {
+  //       path: '',
+  //       redirectTo: '/dashboard',
+  //       pathMatch: 'full'
+  //     },
+  //     {
+  //       path: '',
+  //       loadChildren:
+  //         () => import('./material-component/material.module').then(m => m.MaterialComponentsModule)
+  //     },
+  //     {
+  //       path: 'dashboard',
+  //       loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  //     }
+  //   ]
+  // }
   {
     path: '',
-    component: FullComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: '',
-        loadChildren:
-          () => import('./material-component/material.module').then(m => m.MaterialComponentsModule)
-      },
-      {
-        path: 'dashboard',
-        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
-      }
-    ]
+    component: LoginComponent
   }
 ];
