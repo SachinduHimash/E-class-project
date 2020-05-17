298b35a00ed888e8384eec78d588e4435aaa21b3
isuru1221 <isurusandaruwann1221@gmail.com>
2020-05-17 21:51:13 +0530

    Link sidebar with user profile

--- Admin Panel/src/app/layouts/full/sidebar/sidebar.component.ts
+++ Admin Panel/src/app/layouts/full/sidebar/sidebar.component.ts
@@ -1,36 +1,49 @@
-import {
-  ChangeDetectorRef,
-  Component,
-  NgZone,
-  OnDestroy,
-  ViewChild,
-  HostListener,
-  Directive,
-  AfterViewInit
-} from '@angular/core';
-import { MediaMatcher } from '@angular/cdk/layout';
-import { MenuItems } from '../../../shared/menu-items/menu-items';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {MenuItems} from '../../../shared/menu-items/menu-items';
+import {AngularFireAuth} from "@angular/fire/auth";
+import {AngularFirestore} from "@angular/fire/firestore";
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: []
 })
-export class AppSidebarComponent implements OnDestroy {
+export class AppSidebarComponent implements OnDestroy, OnInit {
   mobileQuery: MediaQueryList;
-
+  user;
   private _mobileQueryListener: () => void;
 
   constructor(
     changeDetectorRef: ChangeDetectorRef,
     media: MediaMatcher,
-    public menuItems: MenuItems
+    public menuItems: MenuItems,
+    private _af: AngularFirestore,
+    private _auth: AngularFireAuth
   ) {
     this.mobileQuery = media.matchMedia('(min-width: 768px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
+  ngOnInit(): void {
+    this.getUserDetails();
+  }
+
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  getUserDetails() {
+    this._auth.currentUser.then((user) => {
+      if (user) {
+        this._af.collection('users')
+          .doc(user.uid)
+          .valueChanges()
+          .subscribe(r => {
+            this.user = r;
+          });
+      }
+    });
   }
 }
