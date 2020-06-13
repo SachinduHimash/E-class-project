90f04c5a2f25aec962a32451238fd1298495fccf
SachinduHimash <himash1997@gmail.com>
2020-06-13 08:47:45 +0530

    Adding login page

---
+++ studentapp/lib/screens/welcome_page.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+class WelcomePage extends  StatefulWidget {
+  WelcomePage({Key key}) : super(key: key);
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _autoValidate = false;
+  String _userID;
+  String _password;
+  
+
+
+  @override
+  Widget build(BuildContext context) {
+   final _height=MediaQuery.of(context).size.height;
+   final _width=MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/background.png"),
+              fit: BoxFit.fill)
+          ),
+          width: _width,
+          height: _height,
+          child: SafeArea(
+           child: Column(
+            children: <Widget>[
+            SizedBox(
+              height: _height*0.03,
+            ),
+            Container(
+                     
+                     child:Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        AutoSizeText("Welcome",
+                        style:TextStyle(color: Colors.blue[600],fontSize: _height*0.09)),
+                        SizedBox(height: _height*0.02,),
+                        AutoSizeText("Please login to access your paper",
+                        maxLines: 2,
+                        style:TextStyle(color: Colors.blue[600],fontSize: _height*0.035))
+                      ]
+                     )
+                    ),
+            SizedBox(
+                     height:_height*0.41,
+                   ),
+            Form(
+              key: _formKey,
+              autovalidate: _autoValidate,
+              child: Column(
+                children: <Widget>[
+                   Container(
+                     margin: EdgeInsets.only(bottom:_height*0.01),
+                     width: _width*0.75 ,
+                     color: Colors.lightBlue[50],
+                     child:Row(
+                       children: <Widget>[
+                          SizedBox(width:_width*0.05),
+                          Icon(
+                            Icons.person,
+                            color: Colors.blue[900],),
+                          SizedBox(width:_width*0.05),
+                          Container(
+                   
+                            width: _width*0.53,
+                            child:TextFormField(
+                             keyboardType: TextInputType.emailAddress,
+                             decoration: InputDecoration(
+                             labelText: 'UserID',
+                             fillColor: Colors.blue,
+                             ),
+                             validator: validateUserId,
+                              onSaved: (String val) {
+                                _userID = val;
+                              },
+                            ),
+                          )
+                        ]
+                     )
+                   ),
+            
+                   Container(
+                     width:_width*0.75 ,
+                     margin: EdgeInsets.only(bottom:_height*0.02),
+                     color: Colors.lightBlue[50],
+                     child:Row(
+                       children: <Widget>[
+                       SizedBox(width:_width*0.05),
+                       Icon(
+                          Icons.lock,
+                          color: Colors.blue[900],),
+                       SizedBox(width:_width*0.05),
+                       Container(
+                          width:_width*0.53,
+                          child:TextFormField(
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                            labelText: 'Password',
+                            fillColor: Colors.blue,
+                            
+                          ),
+                          validator:validatePassword,
+                            onSaved: (String val) {
+                            _password = val;}
+                          ),
+                        )
+                       ]
+                     )
+                   ),
+                   RaisedButton(
+                     padding: EdgeInsets.fromLTRB(_width*0.1,_height*0.02,_width*0.1,_height*0.02),
+                     shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(30)),
+                     onPressed: _submit,
+                     color: Colors.blue[900],
+                     child: Text(
+                       "Login",
+                       style: TextStyle(
+                         color: Colors.white,
+                         fontSize: 20.0,
+                       ),
+                     ),)
+                ],))
+            
+           ],
+            
+          
+          ),
+        ),
+        ),
+      ),
+      
+    );
+  }
+
+
+  Future<void> _submit() async {
+  if (_formKey.currentState.validate()) {
+
+    _formKey.currentState.save();
+
+    
+      
+    } else {
+    //    If all data are not valid then start auto validation.
+        setState(() {
+          _autoValidate = true;
+        });
+      }
+    }
+    
+     
+}
+String validateUserId(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value))
+      return 'Enter a valid UserID';
+    else
+      return null;
+  }
+
+String validatePassword(String value) {
+    if (value.length < 8)
+      return 'Invalid Password';
+    else
+      return null;
+  }
+
